0ca8779708a0374b2defcc989939b0faa8844289
dgrushetskiy <dgrushetskiy@gmail.com>
2020-06-16 19:28:39 +0300

    create maven project

--- src/main/java/ru/gothmog/app/App.java
+++ src/main/java/ru/gothmog/app/App.java
@@ -1,13 +1,8 @@
 package ru.gothmog.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
